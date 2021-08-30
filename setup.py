e12ed3d4c63855f6da5fa0e6517a63ca44292da6
Ruize Tang <1466040111@qq.com>
2021-08-30 19:31:42 +0800

    + setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+from setuptools import setup
+
+setup(name='tlccmd',
+    version='1.0',
+    author='Ruize Tang',
+    author_email='tangruize97@gmail.com',
+    url='https://github.com/tangruize/tlc-cmd',
+    description='Run TLC in CMD',
+    packages=['.'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: MIT License',
+    ],
+    package_data = {
+        '':['*.ini']
+    },
+    install_requires=['requests', 'psutil'],
+    python_requires='>=3',
+    scripts=['tlcwrapper.py', 'trace_reader.py']
+)
