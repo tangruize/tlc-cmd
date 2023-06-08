4cb50a76568a9d26f69d17b25b502ad9eb1535a2
Ruize Tang <1466040111@qq.com>
2023-06-08 22:21:41 +0800

    improve simulation_summary.py
    add print by n traces option
    add print to log file
    improve reduce: reverse delete result

--- simulation_summary.py
+++ simulation_summary.py
@@ -4,6 +4,7 @@
 import argparse
 import os
 import time
+import sys
 from copy import copy
 from collections import defaultdict
 from trace_reader import TraceReader
@@ -55,7 +56,12 @@ class SimulationSummaryMapper:
 
 # Tasks submmitter, reducer and printer
 class ProgressManager:
-    def __init__(self, nproc, is_delete=False, trace_dir=None, finish_file='MC.out', period=5):
+    def __init__(self, nproc, is_delete=False, trace_dir=None, finish_file='MC.out',
+                 period=5, period_ntrace=0, logfile=None):
+        if logfile is not None:
+            self.logfile = open(logfile, 'w')
+        else:
+            self.logfile = sys.stdout
         if trace_dir is not None:
             os.chdir(trace_dir)
         self.prev_time = 0
@@ -66,6 +72,7 @@ class ProgressManager:
         self.submitted = 0
         self.finish_file = finish_file
         self.period = period
+        self.period_ntrace = period_ntrace
         self.results = []
 
     def reduce(self, data: SimulationSummaryData, reduce_actions=False):
@@ -83,11 +90,22 @@ class ProgressManager:
                 self.data.distinct_actions[value] += 1
                 
     def reduce_result(self, reduce_actions=False):
-        for r in self.results:
+        for i in range(len(self.results)-1, -1, -1):
+            r = self.results[i]
             if r.ready():
                 self.reduce(r.get(), reduce_actions=reduce_actions)
-                self.results.remove(r)
-            self.print_progress()
+                del self.results[i]
+            else:
+                self.print_progress()
+                continue
+            if self.period_ntrace:
+                if len(self.data.processed_files) % self.period_ntrace == 0:
+                    self.print_progress(period=0.1)
+            else:
+                self.print_progress()
+    
+    def print(self, *args, **kwargs):
+        print(*args, **kwargs, file=self.logfile, flush=True)
 
     def print_progress(self, period=None):
         current_time = time.time()
@@ -96,16 +114,16 @@ class ProgressManager:
         if current_time - self.prev_time >= period:
             p_ratio = 0 if self.submitted == 0 else len(self.data.processed_files) / self.submitted
             s_ratio = 0 if self.data.total_states == 0 else len(self.data.states) / self.data.total_states
-            print('Processed: {}/{} ({:.3g}%), distinct/total states: {}/{} ({:.3g}%)'.format(
+            self.print('Processed: {}/{} ({:.3g}%), distinct/total states: {}/{} ({:.3g}%)'.format(
                 len(self.data.processed_files), self.submitted, p_ratio * 100,
                 len(self.data.states), self.data.total_states, s_ratio * 100))
-            if period == 0:
-                print('Diameters:')
+            if period < 0:
+                self.print('Diameters:')
                 for key, value in sorted(self.data.diameters.items(), key=lambda x: x[0]):
-                    print("  {} : {}".format(key, value))
-                print('Actions:')
+                    self.print("  {} : {}".format(key, value))
+                self.print('Actions:')
                 for k in self.data.total_actions:
-                    print(' ', k, ':', self.data.distinct_actions[k], '/', self.data.total_actions[k])
+                    self.print(' ', k, ':', self.data.distinct_actions[k], '/', self.data.total_actions[k])
             self.prev_time = current_time
     
     def map(self, fn):
@@ -119,20 +137,18 @@ class ProgressManager:
         for i in os.listdir():
             if self.is_trace_file(i):
                 self.map(i)
-            # if self.submitted % 1000 == 0:
-            #     self.reduce_result()
             self.print_progress()
         self.pool.close()
-        print('Map finished')
+        self.print('Map finished')
         while True:
             self.reduce_result()
             if len(self.results) == 0:
                 self.reduce(data=None, reduce_actions=True)
                 break
             self.print_progress()
-        print('Reduce finished')
+        self.print('Reduce finished')
         self.pool.join()
-        self.print_progress(period=0)
+        self.print_progress(period=-1)
 
 
 if __name__ == '__main__':
@@ -140,6 +156,9 @@ if __name__ == '__main__':
     parser.add_argument(dest='trace_dir', action='store', help='Trace dir')
     parser.add_argument('-r', dest='remove', action='store_true', help='Remove processed files')
     parser.add_argument('-p', dest='nproc', action='store', type=int, default=cpu_count(), help='Number of processes')
-    arg_parser = parser.parse_args()
-    process_man = ProgressManager(nproc=arg_parser.nproc, is_delete=arg_parser.remove, trace_dir=arg_parser.trace_dir)
+    parser.add_argument('-n', dest='ntrace', action='store', type=int, default=0, help='Print progress every n traces')
+    parser.add_argument('-l', dest='logfile', action='store', help='Log output to file')
+    args = parser.parse_args()
+    process_man = ProgressManager(nproc=args.nproc, is_delete=args.remove, trace_dir=args.trace_dir,
+                                  period_ntrace=args.ntrace, logfile=args.logfile)
     process_man.iterate_dir()
